8e20776e2f5c88b2f24c4a6a54215bab8d930cc1
RussianMartabak <zakyabdurrahman10@gmail.com>
2023-07-07 22:52:18 +0700

    added login functionality

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from data.models import Fund
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
+def login_user(request):
+    balance = getBalance()
+    if request.method == "GET":
+        return render(request, "login.html", {
+            "fund" : format(balance, ',')
+        })
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect("/")
+        else:
+            messages.error(request, "Incorrect username or password. I won't tell you which because I'm lazy lol")
+            return render(request, "login.html", {
+                "fund" : format(balance, ',')
+            })
+
+
+def getBalance():
+    return Fund.objects.get(pk=1).current_fund
