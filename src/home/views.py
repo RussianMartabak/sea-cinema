177ef4a35ef0656df4c00a399bd921a770bd9af9
RussianMartabak <zakyabdurrahman10@gmail.com>
2023-07-08 10:25:17 +0700

    integrated auth to home page

--- src/home/views.py
+++ src/home/views.py
@@ -4,10 +4,15 @@ from data.models import Fund
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
+    logged_in = request.user.is_authenticated
     # get a movie object
     movies = Movie.objects.all()
     balance = Fund.objects.get(pk=1).current_fund
     return render(request, "home.html", {
         "movies" : movies,
-        "fund" : format(balance, ",")
+        "fund" : format(balance, ","),
+        "logged_in" : logged_in,
+        "username" : request.user.username
     })
+
+
